5a1f83d49c68fb961cb546d94593ac6a1f1b10ac
arnabdey73 <arnxdey73@linux.com>
2025-05-26 16:59:22 +0200

    fix: update output directory in vercel.json for static files

--- astro.config.ts
+++ astro.config.ts
@@ -3,7 +3,8 @@ import tailwindcss from "@tailwindcss/vite";
 import sitemap from "@astrojs/sitemap";
 import remarkToc from "remark-toc";
 import remarkCollapse from "remark-collapse";
-import vercel from "@astrojs/vercel/static";
+import vercel from "@astrojs/vercel";
+import react from "@astrojs/react";
 import { SITE } from "./src/config";
 
 // https://astro.build/config
@@ -20,11 +21,11 @@ export default defineConfig({
     sitemap({
       filter: page => SITE.showArchives || !page.endsWith("/archives"),
     }),
+    react(),
   ],
   markdown: {
     remarkPlugins: [remarkToc, [remarkCollapse, { test: "Table of contents" }]],
     shikiConfig: {
-      // For more themes, visit https://shiki.style/themes
       themes: { light: "min-light", dark: "night-owl" },
       wrap: true,
     },
@@ -35,14 +36,11 @@ export default defineConfig({
       exclude: ["@resvg/resvg-js"],
     },
   },
-  experimental: {
-    assets: true
-  },
   image: {
-    service: { 
-      entrypoint: 'astro/assets/services/sharp'
+    service: {
+      entrypoint: '@astrojs/vercel/image'
     },
     domains: ["blog.arnabdey.dev"],
     remotePatterns: [{ protocol: "https" }]
-  },
+  }
 });
